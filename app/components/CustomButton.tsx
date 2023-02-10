383e997c0a69b7540b5ed5e0900010f11ef1ad25
sajidahmed66 <sajidahmedsiddiqui66@gmail.com>
2023-02-10 20:24:59 +0600

    custom button comp

---
+++ app/components/CustomButton.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {StyleSheet, Text, TouchableOpacity} from 'react-native';
+
+import colors from '../config/colors';
+interface ButtonCompProps {
+  title: string;
+  onPress: () => void;
+  color: string;
+}
+const CustomButton: React.FC<ButtonCompProps> = ({title, onPress, color}) => {
+  return (
+    <TouchableOpacity
+      style={[styles.button, {backgroundColor: color}]}
+      onPress={onPress}>
+      <Text style={styles.text}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: colors.primary,
+    borderRadius: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 15,
+    width: '100%',
+    marginVertical: 10,
+  },
+  text: {
+    color: colors.white,
+    fontSize: 18,
+    textTransform: 'uppercase',
+    fontWeight: 'bold',
+  },
+});
+
+export default CustomButton;
--- app/screens/Demo.screens.tsx
+++ app/screens/Demo.screens.tsx
@@ -1,10 +1,21 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import Icon from 'react-native-vector-icons/Entypo';
+import CustomButton from '../components/CustomButton';
+import colors from '../config/colors';
 
 const DemoScreens = () => {
   return (
     <View>
       <Text>DemoScreens</Text>
+      <Icon name="mail" size={30} color="#4ecdc4" />
+      <CustomButton
+        title="login"
+        onPress={() => {
+          console.log('print');
+        }}
+        color={colors.primary}
+      />
     </View>
   );
 };
